2er9ey/sbuild

commit 58c70517a44bd984538caa61a9aff5132ae5f04d

Unshare: preserve HOME environment variable
It has been reported [1] that the unshare backend does not set the HOME
environment variable to /sbuild-nonexistent contrary to other backend.

I tracked the issue to the runuser command, which initializes the HOME,
SHELL, USER, LOGNAME, and PATH environment variables. Fortunately the -p
option allows them to be preserved. However this causes the USER and
LOGNAME environment variables to become undefined. This should not be an
issue for LOGNAME, but is definitely one for USER. To fix that define it
in the call to env, like it is already done for PATH.

[1] https://lists.debian.org/debian-wb-team/2024/05/msg00004.html

lib/Sbuild/ChrootUnshare.pm
@@ -306,7 +306,7 @@ sub _get_exec_argv {
     }
 
     return (
-	'env', 'PATH=' . $self->get_conf('PATH'),
+	'env', 'PATH=' . $self->get_conf('PATH'), "USER=$user",
 	get_unshare_cmd({UNSHARE_FLAGS => $unshare, FORK => 1, IDMAP => $self->get('Uid Gid Map'), LINUX32 => $linux32}), 'sh', '-c', "
 	rootdir=\"\$1\"; shift;
 	user=\"\$1\"; shift;
@@ -339,7 +339,7 @@ sub _get_exec_argv {
 	mount -o rbind /sys \"\$rootdir/sys\";
 	mkdir -p \"\$rootdir/proc\";
 	mount -t proc proc \"\$rootdir/proc\";
-	exec /usr/sbin/chroot \"\$rootdir\" $init /sbin/runuser -u \"\$user\" -- sh -c \"cd \\\"\\\$1\\\" && shift && \\\"\\\$@\\\"\" -- \"\$dir\" \"\$@\";
+	exec /usr/sbin/chroot \"\$rootdir\" $init /sbin/runuser -p -u \"\$user\" -- sh -c \"cd \\\"\\\$1\\\" && shift && \\\"\\\$@\\\"\" -- \"\$dir\" \"\$@\";
 	", '--', $self->get('Session ID'), $user, $dir, @bind_mounts, '--'
     );
 }

commit 1c7c6232d59b7e6a03c18b9849593ef025bae3e2

Unshare: define the LOGNAME environment variable
When adding code to preserve the HOME environment variable, I initially
thought that LOGNAME was not useful. Unfortunately it breaks the build
of globus-gram-job-manager, so let's define it the same way than USER.

Fixes: 58c70517a44b

lib/Sbuild/ChrootUnshare.pm
@@ -306,7 +306,7 @@ sub _get_exec_argv {
     }
 
     return (
-	'env', 'PATH=' . $self->get_conf('PATH'), "USER=$user",
+	'env', 'PATH=' . $self->get_conf('PATH'), "USER=$user", "LOGNAME=$user",
 	get_unshare_cmd({UNSHARE_FLAGS => $unshare, FORK => 1, IDMAP => $self->get('Uid Gid Map'), LINUX32 => $linux32}), 'sh', '-c', "
 	rootdir=\"\$1\"; shift;
 	user=\"\$1\"; shift;